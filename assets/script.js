1373fdb3caecba645f29e549da270d7091585c77
Zac Smart <zac_chaz@outlook.com>
2022-09-21 21:13:37 +1000

    added ajax to get current weather from open weather api, not sure if im using the api correctly need to read docs more

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,25 @@ function initAutocomplete() {
 }
 window.initAutocomplete = initAutocomplete;
 
+
+
 function addCity(newCitySearch) {
-    var newCityEl = $('<button>').addClass("li")
+    var newCityEl = $('<button>').addClass("list-group-item")
+    newCityEl.text(newCityEl);
+    //  this hould append new el to city list
+    $("city-list").append(newCityEl);
 }
+
+var info = {
+    URL: "https://api.openweathermap.org/data/2.5/uvi?lat=+latitude+&lon=+longitude+&appid=c10049a3acdd6edc8eb4500188f2fafd;"
+};
+
+$.ajax(info).then(function(response) {
+    var temperature = response.main.temp + "°C";
+    var humidity = response.main.humidity = "%";
+    var windSpeed = response.main.wind.speed = "km/h";
+    $("#forecast-city-header").text(forecastHeader);
+    $("#temperature").text(temperature);
+    $("#humidity").text(humidity);
+    $("#wind-speed").text(windSpeed);
+});
