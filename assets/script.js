61a3dd031bdafaad0f3a8240cd0df6be9392cf29
Zac Smart <zac_chaz@outlook.com>
2022-09-21 14:16:31 +1000

    fixed syntax error in extended forecast class, re did search button HTML to add google autocomplete API added autocomplete function

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
+let autocomplete;
+
+function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'),
+        {
+            fields: ['place_id', 'geometry', 'name']
+        });
+    
+}
+window.initAutocomplete = initAutocomplete;
+
 function addCity(newCitySearch) {
     var newCityEl = $('<button>').addClass("li")
 }
